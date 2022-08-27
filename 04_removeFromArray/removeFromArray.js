b55d2b9a79f5fa730d4955bdc1a66e8f45be554d
Yung <neiklotrr@gmail.com>
2022-08-27 16:07:55 +0300

    Do not understand

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    array = args[0]
+    my_array = array.filter(item => !args.includes(item))
+    return my_array
 };
 
 // Do not edit below this line
