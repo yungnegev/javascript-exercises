456e214b9d6b80de91cf8def47102d10d95b84f9
Yung <neiklotrr@gmail.com>
2022-08-27 17:12:04 +0300

    Attempted.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    let ans = [];
+    for (let i = start; i <= end; i++) {
+        ans.push(i);
+        } 
+    answer = ans.reduce((a, b) => a + b, 0)
+    
+    if(start && end === NaN){
+        return 'ERROR'
+    }
+
+    return answer
 };
 
 // Do not edit below this line
