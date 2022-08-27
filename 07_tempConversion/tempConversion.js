c5d03b98ab4d02b628c25afd4db79908d615515e
Yung <neiklotrr@gmail.com>
2022-08-27 18:07:32 +0300

    Solved. (by looking at my older code)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) / 1.8
+  rounded = Math.round(c * 10) / 10
+  return rounded
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = (c* 1.8) + 32
+  rounded = Math.round(f * 10) / 10
+  return rounded
 };
 
 // Do not edit below this line
