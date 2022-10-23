cc2e4faa1ce25504787b398424954867dcdb96f6
Yung <neiklotrr@gmail.com>
2022-10-23 16:24:38 +0300

    Solved. (dank)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(objArray) {
+    let yearsAlive = []
+    for(x of objArray){
+        if (x.yearOfDeath == undefined){
+            x.yearOfDeath = 2022
+        }
+        yearsAlive.push(x.yearOfDeath - x.yearOfBirth)
+    }
+    let oldestYear = Math.max(...yearsAlive)
+    let pos = yearsAlive.indexOf(oldestYear)
+    return objArray[pos]
+}
 // Do not edit below this line
 module.exports = findTheOldest;
---
+++ 12_findTheOldest/tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
