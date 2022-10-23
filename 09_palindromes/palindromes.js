1505698b2816897bd6226135b2225fb5f93a3e71
Yung <neiklotrr@gmail.com>
2022-10-23 11:21:17 +0300

    Solved.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (anStr) {
+    let str = anStr.toLowerCase()
+    let reversed = str.split("").reverse().join("")
+    let replaced = reversed.replace(/[" ".,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    let replacedStr = str.replace(/[" ".,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    if(replacedStr == replaced){
+        return true
+    }else{
+        return false
+    }
 };
 
 // Do not edit below this line
